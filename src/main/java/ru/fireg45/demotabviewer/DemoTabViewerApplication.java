7e101562c6d5a6fc1c638f83f0b172082f71ef09
FireG45 <cace.200218@gmail.com>
2023-08-03 13:47:13 +0300

    init

---
+++ src/main/java/ru/fireg45/demotabviewer/DemoTabViewerApplication.java
@@ -0,0 +1,13 @@
+package ru.fireg45.demotabviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTabViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTabViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fireg45/demotabviewer/DemoTabViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fireg45.demotabviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTabViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
